3a78fa7c62780e78a17325292ca4f243639ed52e
karim <karimel-kady>
2026-06-11 17:03:49 +0300

    update

--- family-meds-app/babel.config.js
+++ family-meds-app/babel.config.js
@@ -2,8 +2,8 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      'react-native-reanimated/plugin',
-    ],
+    // Note: do NOT add react-native-reanimated/plugin here for SDK 54.
+    // babel-preset-expo already includes the correct reanimated transforms.
+    // Adding it manually causes a missing 'react-native-worklets/plugin' error.
   };
 };
